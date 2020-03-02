f64e2d339063aaa63a26a4505b3fa2dce6589bfa
kahverengi <ahmet.duruer@yandex.com>
2020-03-02 10:49:00 +0100

    Remove cookie handler (deprecated: not needed anymore)

--- src/main/kotlin/com/panomc/platform/di/module/RouterModule.kt
+++ src/main/kotlin/com/panomc/platform/di/module/RouterModule.kt
@@ -39,12 +39,10 @@ import io.vertx.core.Vertx
 import io.vertx.core.http.HttpMethod
 import io.vertx.ext.web.Router
 import io.vertx.ext.web.handler.BodyHandler
-import io.vertx.ext.web.handler.CookieHandler
 import io.vertx.ext.web.handler.CorsHandler
 import io.vertx.ext.web.handler.SessionHandler
 import io.vertx.ext.web.sstore.LocalSessionStore
 import javax.inject.Singleton
-
 
 @Module
 class RouterModule(private val mVertx: Vertx) {
@@ -140,7 +138,6 @@ class RouterModule(private val mVertx: Vertx) {
 
     private fun init(router: Router) {
         router.route().handler(BodyHandler.create())
-        router.route().handler(CookieHandler.create())
         router.route().handler(SessionHandler.create(LocalSessionStore.create(mVertx)))
 
         val allowedHeaders: MutableSet<String> = HashSet()
