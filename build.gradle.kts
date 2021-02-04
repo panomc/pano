00a581d196db5abbcbcdc42aa411f1bacc7803eb
kahverengi <ahmet.duruer@yandex.com>
2021-02-04 19:03:57 +0300

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     java
-    kotlin("jvm") version "1.4.10"
-    kotlin("kapt") version "1.4.10"
-    id("io.vertx.vertx-plugin") version "1.1.3"
+    kotlin("jvm") version "1.4.30"
+    kotlin("kapt") version "1.4.30"
+    id("io.vertx.vertx-plugin") version "1.2.0"
 }
 
 group = "com.panomc.platform"
@@ -42,8 +42,8 @@ dependencies {
     implementation("org.slf4j:slf4j-simple:1.7.30")
 
     // dagger 2x
-    implementation("com.google.dagger:dagger:2.29.1")
-    kapt("com.google.dagger:dagger-compiler:2.29.1")
+    implementation("com.google.dagger:dagger:2.31.2")
+    kapt("com.google.dagger:dagger-compiler:2.31.2")
 
     // recaptcha v2 1.0.3
     implementation("com.github.triologygmbh:reCAPTCHA-V2-java:1.0.4")
