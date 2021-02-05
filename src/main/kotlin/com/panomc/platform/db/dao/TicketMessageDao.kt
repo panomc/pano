b8b30d9f040e6514152d6fff0515477461273026
kahverengi <ahmet.duruer@yandex.com>
2021-02-05 17:10:16 +0300

    Add getByTicketIDAndPage method to TicketMessageDao

--- src/main/kotlin/com/panomc/platform/db/dao/TicketMessageDao.kt
+++ src/main/kotlin/com/panomc/platform/db/dao/TicketMessageDao.kt
@@ -2,7 +2,15 @@ package com.panomc.platform.db.dao
 
 import com.panomc.platform.db.Dao
 import com.panomc.platform.db.model.TicketMessage
+import io.vertx.core.AsyncResult
+import io.vertx.ext.sql.SQLConnection
 
 @Suppress("PARAMETER_NAME_CHANGED_ON_OVERRIDE")
 interface TicketMessageDao : Dao<TicketMessage> {
+    fun getByTicketIDAndPage(
+        ticketID: Int,
+        page: Int,
+        sqlConnection: SQLConnection,
+        handler: (messages: List<TicketMessage>?, asyncResult: AsyncResult<*>) -> Unit
+    )
 }
--- src/main/kotlin/com/panomc/platform/db/entity/TicketMessageDaoImpl.kt
+++ src/main/kotlin/com/panomc/platform/db/entity/TicketMessageDaoImpl.kt
@@ -2,8 +2,11 @@ package com.panomc.platform.db.entity
 
 import com.panomc.platform.db.DaoImpl
 import com.panomc.platform.db.dao.TicketMessageDao
+import com.panomc.platform.db.model.TicketMessage
 import io.vertx.core.AsyncResult
+import io.vertx.core.json.JsonArray
 import io.vertx.ext.sql.SQLConnection
+import java.util.*
 
 class TicketMessageDaoImpl(override val tableName: String = "ticket_message") : DaoImpl(), TicketMessageDao {
     override fun init(): (sqlConnection: SQLConnection, handler: (asyncResult: AsyncResult<*>) -> Unit) -> SQLConnection =
@@ -23,4 +26,35 @@ class TicketMessageDaoImpl(override val tableName: String = "ticket_message") : 
                 handler.invoke(it)
             }
         }
+
+    override fun getByTicketIDAndPage(
+        ticketID: Int,
+        page: Int,
+        sqlConnection: SQLConnection,
+        handler: (messages: List<TicketMessage>?, asyncResult: AsyncResult<*>) -> Unit
+    ) {
+        val query =
+            "SELECT id, user_id, ticket_id, message, `date` FROM `${getTablePrefix() + tableName}` ORDER BY id DESC LIMIT 10 OFFSET ${(page - 1) * 10}"
+        sqlConnection.queryWithParams(query, JsonArray()) { queryResult ->
+            if (queryResult.succeeded()) {
+                val messages = mutableListOf<TicketMessage>()
+
+                if (queryResult.result().results.size > 0) {
+                    messages.add(
+                        TicketMessage(
+                            id = queryResult.result().results[0].getInteger(0),
+                            userID = queryResult.result().results[0].getInteger(1),
+                            ticketID = queryResult.result().results[0].getInteger(2),
+                            message = String(
+                                Base64.getDecoder().decode(queryResult.result().results[0].getString(3).toByteArray())
+                            ),
+                            date = queryResult.result().results[0].getString(4)
+                        )
+                    )
+                }
+                handler.invoke(messages, queryResult)
+            } else
+                handler.invoke(null, queryResult)
+        }
+    }
 }
