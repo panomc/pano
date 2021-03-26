d9385758272e8ea5a2d5f274e7a624d52aa19d37
kahverengi <ahmet.duruer@yandex.com>
2021-03-26 14:55:18 +0300

    Add getPermissionGroupPerms to PermissionGroupPermsDao

--- src/main/kotlin/com/panomc/platform/db/dao/PermissionGroupPermsDao.kt
+++ src/main/kotlin/com/panomc/platform/db/dao/PermissionGroupPermsDao.kt
@@ -2,6 +2,13 @@ package com.panomc.platform.db.dao
 
 import com.panomc.platform.db.Dao
 import com.panomc.platform.db.model.PermissionGroupPerms
+import io.vertx.core.AsyncResult
+import io.vertx.sqlclient.SqlConnection
 
 @Suppress("PARAMETER_NAME_CHANGED_ON_OVERRIDE")
-interface PermissionGroupPermsDao : Dao<PermissionGroupPerms>
+interface PermissionGroupPermsDao : Dao<PermissionGroupPerms> {
+    fun getPermissionGroupPerms(
+        sqlConnection: SqlConnection,
+        handler: (permissionGroupPerms: List<PermissionGroupPerms>?, asyncResult: AsyncResult<*>) -> Unit
+    )
+}
--- src/main/kotlin/com/panomc/platform/db/entity/PermissionGroupPermsDaoImpl.kt
+++ src/main/kotlin/com/panomc/platform/db/entity/PermissionGroupPermsDaoImpl.kt
@@ -2,11 +2,15 @@ package com.panomc.platform.db.entity
 
 import com.panomc.platform.db.DaoImpl
 import com.panomc.platform.db.dao.PermissionGroupPermsDao
+import com.panomc.platform.db.model.PermissionGroupPerms
 import io.vertx.core.AsyncResult
+import io.vertx.sqlclient.Row
+import io.vertx.sqlclient.RowSet
 import io.vertx.sqlclient.SqlConnection
 
-class PermissionGroupPermsDaoImpl(override val tableName: String = "permission_group_perms") : DaoImpl(),
-    PermissionGroupPermsDao {
+class PermissionGroupPermsDaoImpl(
+    override val tableName: String = "permission_group_perms"
+) : DaoImpl(), PermissionGroupPermsDao {
     override fun init(): (sqlConnection: SqlConnection, handler: (asyncResult: AsyncResult<*>) -> Unit) -> Unit =
         { sqlConnection, handler ->
             sqlConnection
@@ -24,4 +28,27 @@ class PermissionGroupPermsDaoImpl(override val tableName: String = "permission_g
                     handler.invoke(it)
                 }
         }
+
+    override fun getPermissionGroupPerms(
+        sqlConnection: SqlConnection,
+        handler: (permissionGroupPerms: List<PermissionGroupPerms>?, asyncResult: AsyncResult<*>) -> Unit
+    ) {
+        val query =
+            "SELECT `id`, `permission_id`, `permission_group_id` FROM `${getTablePrefix() + tableName}`"
+
+        sqlConnection
+            .preparedQuery(query)
+            .execute { queryResult ->
+                if (queryResult.succeeded()) {
+                    val rows: RowSet<Row> = queryResult.result()
+
+                    val permissionGroupPerms = rows.map { row ->
+                        PermissionGroupPerms(row.getInteger(0), row.getInteger(1), row.getInteger(2))
+                    }
+
+                    handler.invoke(permissionGroupPerms, queryResult)
+                } else
+                    handler.invoke(null, queryResult)
+            }
+    }
 }
