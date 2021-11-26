f44e2c76866520fc99149ca8248684c2eb0d265b
kahverengi <ahmet.duruer@yandex.com>
2021-11-26 10:45:24 +0100

    Add giving error instead of rerouting to root when it's already installed in SetupApi

--- src/main/kotlin/com/panomc/platform/model/SetupApi.kt
+++ src/main/kotlin/com/panomc/platform/model/SetupApi.kt
@@ -1,12 +1,13 @@
 package com.panomc.platform.model
 
+import com.panomc.platform.ErrorCode
 import io.vertx.core.Handler
 import io.vertx.ext.web.RoutingContext
 
 abstract class SetupApi : Api() {
     override fun getHandler() = Handler<RoutingContext> { context ->
         if (setupManager.isSetupDone()) {
-            context.reroute("/")
+            sendResult(Error(ErrorCode.PLATFORM_ALREADY_INSTALLED), context)
 
             return@Handler
         }
