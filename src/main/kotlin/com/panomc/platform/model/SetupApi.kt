da86cbb67d6f8ce55c21a0a0637b688fcda76319
kahverengi <ahmet.duruer@yandex.com>
2021-11-26 10:19:01 +0100

    Remove unnecessary dependency injection in SetupApi

--- src/main/kotlin/com/panomc/platform/model/SetupApi.kt
+++ src/main/kotlin/com/panomc/platform/model/SetupApi.kt
@@ -1,15 +1,9 @@
 package com.panomc.platform.model
 
-import com.panomc.platform.Main
 import io.vertx.core.Handler
 import io.vertx.ext.web.RoutingContext
 
 abstract class SetupApi : Api() {
-    init {
-        @Suppress("LeakingThis")
-        Main.getComponent().inject(this)
-    }
-
     override fun getHandler() = Handler<RoutingContext> { context ->
         if (setupManager.isSetupDone()) {
             context.reroute("/")
