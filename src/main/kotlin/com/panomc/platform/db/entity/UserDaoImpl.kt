fc869786cd7e59f7b354d7f78b1441e90c94b0a9
kahverengi <ahmet.duruer@yandex.com>
2021-12-29 20:42:10 +0300

    Add mc_uuid column in UserDaoImpl

--- src/main/kotlin/com/panomc/platform/db/entity/UserDaoImpl.kt
+++ src/main/kotlin/com/panomc/platform/db/entity/UserDaoImpl.kt
@@ -31,6 +31,7 @@ class UserDaoImpl(override val tableName: String = "user") : DaoImpl(), UserDao 
                               `register_date` BIGINT(20) NOT NULL,
                               `email_verified` int(1) NOT NULL DEFAULT 0,
                               `banned` int(1) NOT NULL DEFAULT 0,
+                              `mc_uuid` varchar(255) NOT NULL DEFAULT '',
                               PRIMARY KEY (`id`)
                             ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='User Table';
                         """
