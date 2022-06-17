3046063f16afba169c8cedc5d1f2acc8326a97a2
kahverengi <ahmet@duruer.dev>
2022-06-17 08:13:22 +0200

    Fix wrong setting type enum value for "website" type

--- src/main/kotlin/com/panomc/platform/util/SettingType.kt
+++ src/main/kotlin/com/panomc/platform/util/SettingType.kt
@@ -2,7 +2,7 @@ package com.panomc.platform.util
 
 enum class SettingType(val type: String, val value: Int) {
     GENERAL("general", 0),
-    WEBSITE("WEBSITE", 1);
+    WEBSITE("website", 1);
 
     override fun toString(): String {
         return type
