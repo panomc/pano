fb6be5e9494dab178a6c3e7db2b2619b64a7cd3c
kahverengi <ahmet@duruer.dev>
2022-07-31 22:53:37 +0300

    Add saving mail configuration on step 3 in NextStepAPI

--- src/main/kotlin/com/panomc/platform/route/api/setup/step/NextStepAPI.kt
+++ src/main/kotlin/com/panomc/platform/route/api/setup/step/NextStepAPI.kt
@@ -9,9 +9,9 @@ import com.panomc.platform.model.Successful
 import com.panomc.platform.util.SetupManager
 import io.vertx.ext.web.RoutingContext
 import io.vertx.ext.web.validation.ValidationHandler
-import io.vertx.ext.web.validation.builder.Bodies
+import io.vertx.ext.web.validation.builder.Bodies.json
 import io.vertx.json.schema.SchemaParser
-import io.vertx.json.schema.common.dsl.Schemas
+import io.vertx.json.schema.common.dsl.Schemas.*
 
 @Endpoint
 class NextStepAPI(
@@ -25,16 +25,19 @@ class NextStepAPI(
     override fun getValidationHandler(schemaParser: SchemaParser): ValidationHandler =
         ValidationHandler.builder(schemaParser)
             .body(
-                Bodies.json(
-                    Schemas.objectSchema()
-                        .property("step", Schemas.intSchema())
-                        .optionalProperty("websiteName", Schemas.stringSchema())
-                        .optionalProperty("websiteDescription", Schemas.stringSchema())
-                        .optionalProperty("host", Schemas.stringSchema())
-                        .optionalProperty("dbName", Schemas.stringSchema())
-                        .optionalProperty("username", Schemas.stringSchema())
-                        .optionalProperty("password", Schemas.stringSchema())
-                        .optionalProperty("prefix", Schemas.stringSchema())
+                json(
+                    objectSchema()
+                        .property("step", intSchema())
+                        .optionalProperty("websiteName", stringSchema())
+                        .optionalProperty("websiteDescription", stringSchema())
+                        .optionalProperty("host", stringSchema())
+                        .optionalProperty("port", intSchema())
+                        .optionalProperty("address", stringSchema())
+                        .optionalProperty("dbName", stringSchema())
+                        .optionalProperty("username", stringSchema())
+                        .optionalProperty("password", stringSchema())
+                        .optionalProperty("prefix", stringSchema())
+                        .optionalProperty("useSSL", booleanSchema())
                 )
             )
             .build()
@@ -44,44 +47,61 @@ class NextStepAPI(
         val data = parameters.body().jsonObject
 
         val clientStep = data.getInteger("step")
+        val websiteName = data.getString("websiteName")
+        val websiteDescription = data.getString("websiteDescription")
+        val host = data.getString("host")
+        val port = data.getInteger("port")
+        val address = data.getString("address")
+        val dbName = data.getString("dbName")
+        val username = data.getString("username")
+        val password = data.getString("password")
+        val prefix = data.getString("prefix")
+        val useSSL = data.getBoolean("useSSL")
 
         if (clientStep == setupManager.getStep()) {
             var passStep = false
 
             if (clientStep == 0)
                 passStep = true
-            else if (clientStep == 1 && !data.getString("websiteName")
-                    .isNullOrEmpty() && !data.getString("websiteDescription").isNullOrEmpty()
+            else if (clientStep == 1 && !websiteName
+                    .isNullOrEmpty() && !websiteDescription.isNullOrEmpty()
             ) {
-                configManager.getConfig().put("website-name", data.getString("websiteName"))
-                configManager.getConfig().put("website-description", data.getString("websiteDescription"))
+                configManager.getConfig().put("website-name", websiteName)
+                configManager.getConfig().put("website-description", websiteDescription)
 
                 passStep = true
             } else if (
                 clientStep == 2 &&
-                !data.getString("host").isNullOrEmpty() &&
-                !data.getString("dbName").isNullOrEmpty() &&
-                !data.getString("username").isNullOrEmpty()
+                !host.isNullOrEmpty() &&
+                !dbName.isNullOrEmpty() &&
+                !username.isNullOrEmpty()
             ) {
                 val databaseOptions = configManager.getConfig().getJsonObject("database")
 
-                databaseOptions.put("host", data.getString("host"))
-                databaseOptions.put("name", data.getString("dbName"))
-                databaseOptions.put(
-                    "username",
-                    data.getString("username")
-                )
-                databaseOptions.put(
-                    "password",
-                    if (data.getString("password").isNullOrEmpty()) "" else data.getString("password")
-                )
-                databaseOptions.put(
-                    "prefix",
-                    if (data.getString("prefix").isNullOrEmpty()) "" else data.getString("prefix")
-                )
+                databaseOptions.put("host", host)
+                databaseOptions.put("name", dbName)
+                databaseOptions.put("username", username)
+                databaseOptions.put("password", if (password.isNullOrEmpty()) "" else password)
+                databaseOptions.put("prefix", if (prefix.isNullOrEmpty()) "" else prefix)
 
                 passStep = true
-            } else if (clientStep == 3) {
+            } else if (clientStep == 3 &&
+                !host.isNullOrEmpty() &&
+                port != null &&
+                !address.isNullOrEmpty() &&
+                !username.isNullOrEmpty() &&
+                !password.isNullOrEmpty() &&
+                useSSL != null
+            ) {
+                val mailConfiguration = configManager.getConfig().getJsonObject("email")
+
+                mailConfiguration.put("address", address)
+                mailConfiguration.put("host", host)
+                mailConfiguration.put("port", port)
+                mailConfiguration.put("username", username)
+                mailConfiguration.put("password", password)
+                mailConfiguration.put("SSL", useSSL)
+
                 passStep = true
             }
 
