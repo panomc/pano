46d7c0df7be9199a6e6179986b2ed728a1496285
kahverengi <ahmet@duruer.dev>
2022-08-01 13:59:01 +0300

    Add language field to config file with migration from 7 to 8

--- src/main/kotlin/com/panomc/platform/config/ConfigManager.kt
+++ src/main/kotlin/com/panomc/platform/config/ConfigManager.kt
@@ -17,7 +17,7 @@ import java.io.File
 class ConfigManager(vertx: Vertx, private val logger: Logger, applicationContext: AnnotationConfigApplicationContext) {
 
     companion object {
-        private const val CONFIG_VERSION = 7
+        private const val CONFIG_VERSION = 8
 
         private val DEFAULT_CONFIG by lazy {
             val key = KeyGeneratorUtil.generateJWTKeys()
@@ -26,6 +26,7 @@ class ConfigManager(vertx: Vertx, private val logger: Logger, applicationContext
                 mapOf(
                     "config-version" to CONFIG_VERSION,
                     "development-mode" to true,
+                    "language" to "en-US",
 
                     "website-name" to "",
                     "website-description" to "",
