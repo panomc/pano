0a77f2daa82d60a5ecde4d8719c7420d66f50f3a
kahverengi <ahmet@duruer.dev>
2022-10-16 19:08:16 +0300

    Add PanelUnbanPlayerAPI

---
+++ src/main/kotlin/com/panomc/platform/route/api/panel/playerDetail/PanelUnbanPlayerAPI.kt
@@ -0,0 +1,57 @@
+package com.panomc.platform.route.api.panel.playerDetail
+
+import com.panomc.platform.ErrorCode
+import com.panomc.platform.annotation.Endpoint
+import com.panomc.platform.db.DatabaseManager
+import com.panomc.platform.model.*
+import com.panomc.platform.util.AuthProvider
+import com.panomc.platform.util.SetupManager
+import io.vertx.ext.web.RoutingContext
+import io.vertx.ext.web.validation.ValidationHandler
+import io.vertx.ext.web.validation.builder.Parameters
+import io.vertx.ext.web.validation.builder.ValidationHandlerBuilder
+import io.vertx.json.schema.SchemaParser
+import io.vertx.json.schema.common.dsl.Schemas.stringSchema
+
+@Endpoint
+class PanelUnbanPlayerAPI(
+    setupManager: SetupManager,
+    authProvider: AuthProvider,
+    private val databaseManager: DatabaseManager,
+) : PanelApi(setupManager, authProvider) {
+    override val routeType = RouteType.POST
+
+    override val routes = arrayListOf("/api/panel/players/:username/unban")
+
+    override fun getValidationHandler(schemaParser: SchemaParser): ValidationHandler =
+        ValidationHandlerBuilder.create(schemaParser)
+            .pathParameter(Parameters.param("username", stringSchema()))
+            .build()
+
+    override suspend fun handler(context: RoutingContext): Result {
+        val parameters = getParameters(context)
+
+        val username = parameters.pathParameter("username").string
+
+        val sqlConnection = createConnection(databaseManager, context)
+
+        val isExists = databaseManager.userDao.isExistsByUsername(username, sqlConnection)
+
+        if (!isExists) {
+            throw Error(ErrorCode.NOT_EXISTS)
+        }
+
+        val userId =
+            databaseManager.userDao.getUserIdFromUsername(username, sqlConnection) ?: throw Error(ErrorCode.NOT_EXISTS)
+
+        val isBanned = databaseManager.userDao.isBanned(userId, sqlConnection)
+
+        if (!isBanned) {
+            throw Error(ErrorCode.NOT_BANNED)
+        }
+
+        databaseManager.userDao.unbanPlayer(userId, sqlConnection)
+
+        return Successful()
+    }
+}
