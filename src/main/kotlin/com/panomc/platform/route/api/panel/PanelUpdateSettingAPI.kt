3631aa9d4969e7b1c0be3c4a1e2f3ab9b9bcc361
kahverengi <ahmet@duruer.dev>
2022-11-20 23:15:33 +0300

    Add saving file uploads in PanelUpdateSettingAPI

--- src/main/kotlin/com/panomc/platform/route/api/panel/PanelUpdateSettingAPI.kt
+++ src/main/kotlin/com/panomc/platform/route/api/panel/PanelUpdateSettingAPI.kt
@@ -1,5 +1,6 @@
 package com.panomc.platform.route.api.panel
 
+import com.panomc.platform.ErrorCode
 import com.panomc.platform.annotation.Endpoint
 import com.panomc.platform.config.ConfigManager
 import com.panomc.platform.model.PanelApi
@@ -7,6 +8,7 @@ import com.panomc.platform.model.Result
 import com.panomc.platform.model.RouteType
 import com.panomc.platform.model.Successful
 import com.panomc.platform.util.AuthProvider
+import com.panomc.platform.util.FileUploadUtil
 import com.panomc.platform.util.SetupManager
 import com.panomc.platform.util.UpdatePeriod
 import io.vertx.ext.web.RoutingContext
@@ -26,10 +28,49 @@ class PanelUpdateSettingAPI(
 
     override val routes = arrayListOf("/api/panel/settings")
 
+    private val defaultWebsiteUploadPath = "website"
+
+    private val acceptedFileFields = listOf(
+        FileUploadUtil.Field(
+            name = "favicon",
+            fieldConfig = FileUploadUtil.FieldConfig(
+                path = "$defaultWebsiteUploadPath/favicon",
+                acceptedContentTypes = listOf(
+                    "image/x-icon",
+                    "image/vnd.microsoft.icon",
+                    "image/svg+xml",
+                    "image/png",
+                    "image/gif"
+                ),
+                contentTypeError = ErrorCode.FAVICON_WRONG_CONTENT_TYPE,
+                fileSizeError = ErrorCode.FAVICON_EXCEEDS_SIZE,
+                withTempName = false,
+                size = 1024 * 1024 // 1 MB
+            )
+        ),
+        FileUploadUtil.Field(
+            name = "websiteLogo",
+            fieldConfig = FileUploadUtil.FieldConfig(
+                path = "$defaultWebsiteUploadPath/website-logo",
+                acceptedContentTypes = listOf(
+                    "image/x-icon",
+                    "image/vnd.microsoft.icon",
+                    "image/svg+xml",
+                    "image/png",
+                    "image/jpeg"
+                ),
+                contentTypeError = ErrorCode.WEBSITE_LOGO_WRONG_CONTENT_TYPE,
+                fileSizeError = ErrorCode.WEBSITE_LOGO_EXCEEDS_SIZE,
+                withTempName = false,
+                size = 2 * 1024 * 1024 // 2 MB
+            )
+        )
+    )
+
     override fun getValidationHandler(schemaParser: SchemaParser): ValidationHandler =
         ValidationHandlerBuilder.create(schemaParser)
             .body(
-                Bodies.json(
+                Bodies.multipartFormData(
                     objectSchema()
                         .optionalProperty(
                             "updatePeriod",
@@ -49,6 +90,8 @@ class PanelUpdateSettingAPI(
         val parameters = getParameters(context)
         val data = parameters.body().jsonObject
 
+        val fileUploads = context.fileUploads()
+
         val updatePeriod = UpdatePeriod.valueOf(period = data.getString("updatePeriod"))
         val locale = data.getString("locale")
         val websiteName = data.getString("websiteName")
@@ -56,6 +99,16 @@ class PanelUpdateSettingAPI(
         val supportEmail = data.getString("supportEmail")
         val serverIpAddress = data.getString("serverIpAddress")
         val keywords = data.getJsonArray("keywords")
+
+        if (fileUploads.size > 0) {
+            val savedFiles = FileUploadUtil.saveFiles(fileUploads, acceptedFileFields, configManager)
+
+            savedFiles.forEach { savedFile ->
+                val filePathsInConfig = configManager.getConfig().getJsonObject("file-paths")
+
+                filePathsInConfig.put(savedFile.field.name, savedFile.path)
+            }
+        }
 
         if (updatePeriod != null) {
             configManager.getConfig().put("update-period", updatePeriod.period)
