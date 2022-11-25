7bbe0db8e9726a268634992bf225769a21853302
kahverengi <ahmet@duruer.dev>
2022-11-26 01:33:57 +0300

    Add TestSendNotificationAPI

---
+++ src/main/kotlin/com/panomc/platform/route/api/TestSendNotificationAPI.kt
@@ -0,0 +1,39 @@
+package com.panomc.platform.route.api
+
+import com.panomc.platform.annotation.Endpoint
+import com.panomc.platform.db.DatabaseManager
+import com.panomc.platform.db.model.PanelNotification
+import com.panomc.platform.model.*
+import com.panomc.platform.util.AuthProvider
+import com.panomc.platform.util.SetupManager
+import io.vertx.ext.web.RoutingContext
+import io.vertx.ext.web.validation.ValidationHandler
+import io.vertx.ext.web.validation.builder.ValidationHandlerBuilder
+import io.vertx.json.schema.SchemaParser
+
+@Endpoint
+class TestSendNotificationAPI(
+    private val authProvider: AuthProvider,
+    private val databaseManager: DatabaseManager,
+    setupManager: SetupManager
+) : PanelApi(setupManager, authProvider) {
+    override val paths = listOf(Path("/api/testNotification", RouteType.GET))
+
+    override fun getValidationHandler(schemaParser: SchemaParser): ValidationHandler =
+        ValidationHandlerBuilder.create(schemaParser).build()
+
+    override suspend fun handler(context: RoutingContext): Result {
+        val userId = authProvider.getUserIdFromRoutingContext(context)
+
+        val sqlConnection = createConnection(databaseManager, context)
+
+        databaseManager.notificationDao.add(getNotification(userId), sqlConnection)
+
+        return Successful()
+    }
+
+    private fun getNotification(userId: Long) = PanelNotification(
+        userId = userId,
+        typeId = "TEST NOTIFICATION"
+    )
+}
