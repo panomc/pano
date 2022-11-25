d98a5fc37f517f0bf050e74047a2f4344e0d40da
kahverengi <ahmet@duruer.dev>
2022-11-26 00:03:20 +0300

    Add getLast5ByUserId and getCountOfNotReadByUserId methods to NotificationDao

--- src/main/kotlin/com/panomc/platform/db/dao/NotificationDao.kt
+++ src/main/kotlin/com/panomc/platform/db/dao/NotificationDao.kt
@@ -2,6 +2,16 @@ package com.panomc.platform.db.dao
 
 import com.panomc.platform.db.Dao
 import com.panomc.platform.db.model.Notification
+import io.vertx.sqlclient.SqlConnection
 
 interface NotificationDao : Dao<Notification> {
+    suspend fun getLast5ByUserId(
+        userId: Long,
+        sqlConnection: SqlConnection
+    ): List<Notification>
+
+    suspend fun getCountOfNotReadByUserId(
+        userId: Long,
+        sqlConnection: SqlConnection
+    ): Long
 }
--- src/main/kotlin/com/panomc/platform/db/implementation/NotificationDaoImpl.kt
+++ src/main/kotlin/com/panomc/platform/db/implementation/NotificationDaoImpl.kt
@@ -4,8 +4,13 @@ import com.panomc.platform.annotation.Dao
 import com.panomc.platform.db.DaoImpl
 import com.panomc.platform.db.DatabaseManager
 import com.panomc.platform.db.dao.NotificationDao
+import com.panomc.platform.db.model.Notification
+import com.panomc.platform.util.NotificationStatus
 import io.vertx.kotlin.coroutines.await
+import io.vertx.sqlclient.Row
+import io.vertx.sqlclient.RowSet
 import io.vertx.sqlclient.SqlConnection
+import io.vertx.sqlclient.Tuple
 
 @Dao
 class NotificationDaoImpl(databaseManager: DatabaseManager) : DaoImpl(databaseManager, "panel_notification"),
@@ -27,5 +32,36 @@ class NotificationDaoImpl(databaseManager: DatabaseManager) : DaoImpl(databaseMa
             )
             .execute()
             .await()
+    }
+
+    override suspend fun getLast5ByUserId(userId: Long, sqlConnection: SqlConnection): List<Notification> {
+        val query =
+            "SELECT `id`, `user_id`, `type_id`, `date`, `status` FROM `${getTablePrefix() + tableName}` WHERE `user_id` = ? ORDER BY `date` DESC, `id` DESC LIMIT 5"
+
+        val rows: RowSet<Row> = sqlConnection
+            .preparedQuery(query)
+            .execute(
+                Tuple.of(
+                    userId
+                )
+            ).await()
+
+        return Notification.from(rows)
+    }
+
+    override suspend fun getCountOfNotReadByUserId(userId: Long, sqlConnection: SqlConnection): Long {
+        val query =
+            "SELECT count(`id`) FROM `${getTablePrefix() + tableName}` WHERE `user_id` = ? AND `status` = ? ORDER BY `date` DESC, `id` DESC"
+
+        val rows: RowSet<Row> = sqlConnection
+            .preparedQuery(query)
+            .execute(
+                Tuple.of(
+                    userId,
+                    NotificationStatus.NOT_READ,
+                )
+            ).await()
+
+        return rows.toList()[0].getLong(0)
     }
 }
