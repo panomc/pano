abc1602ed6498222e4d4a89e92cd66c061ccf8e9
kahverengi <ahmet@duruer.dev>
2022-11-26 00:57:33 +0300

    Add MarkQuickNotificationsAsReadAPI

---
+++ src/main/kotlin/com/panomc/platform/route/api/notification/MarkQuickNotificationsAsReadAPI.kt
@@ -0,0 +1,39 @@
+package com.panomc.platform.route.api.notification
+
+import com.panomc.platform.annotation.Endpoint
+import com.panomc.platform.db.DatabaseManager
+import com.panomc.platform.model.*
+import com.panomc.platform.util.AuthProvider
+import com.panomc.platform.util.SetupManager
+import io.vertx.ext.web.RoutingContext
+import io.vertx.ext.web.validation.ValidationHandler
+import io.vertx.ext.web.validation.builder.ValidationHandlerBuilder
+import io.vertx.json.schema.SchemaParser
+
+@Endpoint
+class MarkQuickNotificationsAsReadAPI(
+    private val authProvider: AuthProvider,
+    private val databaseManager: DatabaseManager,
+    setupManager: SetupManager
+) : LoggedInApi(setupManager, authProvider) {
+    override val paths = listOf(Path("/api/markQuickNotificationsAsRead", RouteType.POST))
+
+    override fun getValidationHandler(schemaParser: SchemaParser): ValidationHandler =
+        ValidationHandlerBuilder.create(schemaParser).build()
+
+    override suspend fun handler(context: RoutingContext): Result {
+        val userId = authProvider.getUserIdFromRoutingContext(context)
+
+        val sqlConnection = createConnection(databaseManager, context)
+
+        databaseManager.notificationDao.markReadLast5ByUserId(userId, sqlConnection)
+
+        val count = databaseManager.notificationDao.getCountOfNotReadByUserId(userId, sqlConnection)
+
+        return Successful(
+            mutableMapOf(
+                "notificationCount" to count
+            )
+        )
+    }
+}
