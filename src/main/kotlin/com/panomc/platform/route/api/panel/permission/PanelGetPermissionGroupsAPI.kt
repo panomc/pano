89f868e6e86314b10a502445a5e28c81df70429f
kahverengi <ahmet@duruer.dev>
2022-12-02 02:18:29 +0300

    Optimize codes & add returning user count and permission count in PanelGetPermissionGroupsAPI

--- src/main/kotlin/com/panomc/platform/route/api/panel/permission/PanelGetPermissionGroupsAPI.kt
+++ src/main/kotlin/com/panomc/platform/route/api/panel/permission/PanelGetPermissionGroupsAPI.kt
@@ -1,14 +1,19 @@
 package com.panomc.platform.route.api.panel.permission
 
+import com.panomc.platform.ErrorCode
 import com.panomc.platform.annotation.Endpoint
 import com.panomc.platform.db.DatabaseManager
+import com.panomc.platform.db.model.PermissionGroup
 import com.panomc.platform.model.*
 import com.panomc.platform.util.AuthProvider
 import com.panomc.platform.util.SetupManager
 import io.vertx.ext.web.RoutingContext
 import io.vertx.ext.web.validation.ValidationHandler
+import io.vertx.ext.web.validation.builder.Parameters
 import io.vertx.ext.web.validation.builder.ValidationHandlerBuilder
 import io.vertx.json.schema.SchemaParser
+import io.vertx.json.schema.common.dsl.Schemas.numberSchema
+import kotlin.math.ceil
 
 @Endpoint
 class PanelGetPermissionGroupsAPI(
@@ -19,13 +24,79 @@ class PanelGetPermissionGroupsAPI(
     override val paths = listOf(Path("/api/panel/permissionGroups", RouteType.GET))
 
     override fun getValidationHandler(schemaParser: SchemaParser): ValidationHandler =
-        ValidationHandlerBuilder.create(schemaParser).build()
+        ValidationHandlerBuilder.create(schemaParser)
+            .queryParameter(Parameters.optionalParam("page", numberSchema()))
+            .build()
 
     override suspend fun handler(context: RoutingContext): Result {
+        val parameters = getParameters(context)
+        val page = parameters.queryParameter("page")?.long ?: 0L
+
+        val result = mutableMapOf<String, Any?>()
+
         val sqlConnection = createConnection(databaseManager, context)
 
-        val permissionGroups = databaseManager.permissionGroupDao.getPermissionGroups(sqlConnection)
+        val permissionGroupCount = databaseManager.permissionGroupDao.countPermissionGroups(sqlConnection)
+        val permissions = databaseManager.permissionDao.getPermissions(sqlConnection)
 
-        return Successful(mapOf("permissionGroups" to permissionGroups))
+        var totalPage = ceil(permissionGroupCount.toDouble() / 10).toLong()
+
+        if (totalPage < 1)
+            totalPage = 1
+
+        if (page != 0L && (page > totalPage || page < 1)) {
+            throw Error(ErrorCode.PAGE_NOT_FOUND)
+        }
+
+        val permissionGroups = if (page == 0L)
+            databaseManager.permissionGroupDao.getPermissionGroups(sqlConnection)
+        else
+            databaseManager.permissionGroupDao.getPermissionGroupsByPage(page, sqlConnection)
+
+        val permissionGroupList: List<MutableMap<String, Any?>> = permissionGroups.map { permissionGroup ->
+            mutableMapOf(
+                "id" to permissionGroup.id,
+                "name" to permissionGroup.name
+            )
+        }
+
+        val getPermissionGroupData: suspend (PermissionGroup) -> Unit = { permissionGroup ->
+            val foundPermissionGroup = permissionGroupList.find { it["id"] == permissionGroup.id }!!
+
+            val userCount =
+                databaseManager.userDao.getCountOfUsersByPermissionGroupId(permissionGroup.id, sqlConnection)
+            val usernameList =
+                databaseManager.userDao.getUsernamesByPermissionGroupId(permissionGroup.id, 3, sqlConnection)
+            val permissionCount = databaseManager.permissionGroupPermsDao.countPermissionsByPermissionGroupId(
+                permissionGroup.id,
+                sqlConnection
+            )
+
+            foundPermissionGroup["userCount"] = userCount
+            foundPermissionGroup["users"] = usernameList
+            foundPermissionGroup["permissionCount"] = permissionCount
+        }
+
+        permissionGroups.forEach {
+            getPermissionGroupData(it)
+        }
+
+        val permissionGroupPerms = databaseManager.permissionGroupPermsDao.getPermissionGroupPerms(sqlConnection)
+
+        val permissionGroupPermIdListMap = permissionGroupPerms
+            .distinctBy { it.permissionGroupId }
+            .associateBy({ it.permissionGroupId }, { mutableListOf<Long>() })
+
+        permissionGroupPerms.forEach { perm ->
+            permissionGroupPermIdListMap[perm.permissionGroupId]!!.add(perm.permissionId)
+        }
+
+        result["permissionGroups"] = permissionGroupList
+        result["permissionGroupCount"] = permissionGroupCount
+        result["totalPage"] = totalPage
+        result["permissionGroupPerms"] = permissionGroupPermIdListMap
+        result["permissions"] = permissions
+
+        return Successful(result)
     }
 }
