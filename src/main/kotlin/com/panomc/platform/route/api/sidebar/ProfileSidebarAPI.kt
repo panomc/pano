74eef2a17ffdf50513482d9fb2ede7e7a3ca5e7d
kahverengi <ahmet@duruer.dev>
2022-12-02 20:08:26 +0300

    Add ProfileSidebarAPI

---
+++ src/main/kotlin/com/panomc/platform/route/api/sidebar/ProfileSidebarAPI.kt
@@ -0,0 +1,43 @@
+package com.panomc.platform.route.api.sidebar
+
+import com.panomc.platform.annotation.Endpoint
+import com.panomc.platform.db.DatabaseManager
+import com.panomc.platform.model.*
+import com.panomc.platform.util.AuthProvider
+import io.vertx.ext.web.RoutingContext
+import io.vertx.ext.web.validation.ValidationHandler
+import io.vertx.ext.web.validation.builder.ValidationHandlerBuilder
+import io.vertx.json.schema.SchemaParser
+
+@Endpoint
+class ProfileSidebarAPI(private val databaseManager: DatabaseManager, private val authProvider: AuthProvider) : Api() {
+    override val paths = listOf(Path("/api/sidebar/profile", RouteType.GET))
+
+    override fun getValidationHandler(schemaParser: SchemaParser): ValidationHandler =
+        ValidationHandlerBuilder.create(schemaParser)
+            .build()
+
+
+    override suspend fun handler(context: RoutingContext): Result {
+        val response = mutableMapOf<String, Any?>()
+
+        val userId = authProvider.getUserIdFromRoutingContext(context)
+
+        val sqlConnection = createConnection(databaseManager, context)
+
+        val userPermissionGroupId = databaseManager.userDao.getPermissionGroupIdFromUserId(userId, sqlConnection)!!
+
+        var name = ""
+
+        if (userPermissionGroupId != -1L) {
+            val userPermissionGroup =
+                databaseManager.permissionGroupDao.getPermissionGroupById(userPermissionGroupId, sqlConnection)!!
+
+            name = userPermissionGroup.name
+        }
+
+        response["permissionGroupName"] = name
+
+        return Successful(response)
+    }
+}
