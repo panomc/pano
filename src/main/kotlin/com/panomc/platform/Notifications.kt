f84944609bb4cb9a34040bdc5e22df962b28e10b
kahverengi <ahmet@duruer.dev>
2022-12-03 00:02:03 +0300

    Add sending notification in PanelUpdateTicketsAPI

--- src/main/kotlin/com/panomc/platform/Notifications.kt
+++ src/main/kotlin/com/panomc/platform/Notifications.kt
@@ -4,11 +4,12 @@ enum class Notifications {
     ;
 
     enum class UserNotification(val typeId: String, val action: String) {
-        AN_ADMIN_REPLIED_TICKET("AN_ADMIN_REPLIED_TICKET", "AN_ADMIN_REPLIED_TICKET")
+        AN_ADMIN_REPLIED_TICKET("AN_ADMIN_REPLIED_TICKET", "AN_ADMIN_REPLIED_TICKET"),
+        AN_ADMIN_CLOSED_TICKET("AN_ADMIN_CLOSED_TICKET", "AN_ADMIN_CLOSED_TICKET")
     }
 
     enum class PanelNotification(val typeId: String, val action: String) {
         NEW_TICKET("NEW_TICKET", "NEW_TICKET"),
-        NEW_TICKET_MESSAGE("NEW_TICKET_MESSAGE", "NEW_TICKET_MESSAGE");
+        NEW_TICKET_MESSAGE("NEW_TICKET_MESSAGE", "NEW_TICKET_MESSAGE")
     }
 }
--- src/main/kotlin/com/panomc/platform/route/api/panel/ticket/PanelUpdateTicketsAPI.kt
+++ src/main/kotlin/com/panomc/platform/route/api/panel/ticket/PanelUpdateTicketsAPI.kt
@@ -1,10 +1,14 @@
 package com.panomc.platform.route.api.panel.ticket
 
+import com.panomc.platform.ErrorCode
+import com.panomc.platform.Notifications
 import com.panomc.platform.annotation.Endpoint
 import com.panomc.platform.db.DatabaseManager
+import com.panomc.platform.db.model.Notification
 import com.panomc.platform.model.*
 import com.panomc.platform.util.AuthProvider
 import com.panomc.platform.util.SetupManager
+import io.vertx.core.json.JsonObject
 import io.vertx.ext.web.RoutingContext
 import io.vertx.ext.web.validation.ValidationHandler
 import io.vertx.ext.web.validation.builder.Bodies.json
@@ -16,7 +20,7 @@ import io.vertx.json.schema.common.dsl.Schemas.*
 class PanelUpdateTicketsAPI(
     private val databaseManager: DatabaseManager,
     setupManager: SetupManager,
-    authProvider: AuthProvider
+    private val authProvider: AuthProvider
 ) : PanelApi(setupManager, authProvider) {
     override val paths = listOf(Path("/api/panel/tickets", RouteType.PUT))
 
@@ -25,7 +29,7 @@ class PanelUpdateTicketsAPI(
             .body(
                 json(
                     objectSchema()
-                        .property("tickets", arraySchema().items(intSchema()))
+                        .property("tickets", arraySchema().items(numberSchema()))
                         .optionalProperty("status", stringSchema())
                 )
             )
@@ -37,6 +41,8 @@ class PanelUpdateTicketsAPI(
         val selectedTickets = data.getJsonArray("tickets")
         val ticketStatus = data.getString("status")
 
+        val userId = authProvider.getUserIdFromRoutingContext(context)
+
         if (selectedTickets.isEmpty) {
             return Successful()
         }
@@ -44,7 +50,30 @@ class PanelUpdateTicketsAPI(
         if (ticketStatus != null && ticketStatus == "close") {
             val sqlConnection = createConnection(databaseManager, context)
 
+            val areIdListExist =
+                databaseManager.ticketDao.areIdListExist(selectedTickets.map { it.toString().toLong() }, sqlConnection)
+
+            if (!areIdListExist) {
+                throw Error(ErrorCode.SOME_TICKETS_ARENT_EXIST)
+            }
+
             databaseManager.ticketDao.closeTickets(selectedTickets, sqlConnection)
+
+            val username = databaseManager.userDao.getUsernameFromUserId(userId, sqlConnection)
+
+            selectedTickets.map { it.toString().toLong() }.forEach {
+                val ticket = databaseManager.ticketDao.getById(it, sqlConnection)!!
+
+                databaseManager.notificationDao.add(
+                    Notification(
+                        userId = ticket.userId,
+                        typeId = Notifications.UserNotification.AN_ADMIN_CLOSED_TICKET.typeId,
+                        action = Notifications.UserNotification.AN_ADMIN_CLOSED_TICKET.action,
+                        properties = JsonObject().put("id", it).put("whoClosed", username)
+                    ),
+                    sqlConnection
+                )
+            }
         }
 
         return Successful()
