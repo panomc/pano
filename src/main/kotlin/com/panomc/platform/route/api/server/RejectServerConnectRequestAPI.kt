d6e665ebc4f1bfb8c5cbe687d291904b71c3aa39
kahverengi <ahmet@duruer.dev>
2022-12-17 01:36:42 +0300

    Add AcceptServerConnectRequestAPI

---
+++ src/main/kotlin/com/panomc/platform/route/api/server/RejectServerConnectRequestAPI.kt
@@ -0,0 +1,51 @@
+package com.panomc.platform.route.api.server
+
+import com.panomc.platform.ErrorCode
+import com.panomc.platform.annotation.Endpoint
+import com.panomc.platform.db.DatabaseManager
+import com.panomc.platform.model.*
+import com.panomc.platform.server.ServerManager
+import com.panomc.platform.util.AuthProvider
+import com.panomc.platform.util.SetupManager
+import io.vertx.ext.web.RoutingContext
+import io.vertx.ext.web.validation.ValidationHandler
+import io.vertx.ext.web.validation.builder.Parameters.param
+import io.vertx.ext.web.validation.builder.ValidationHandlerBuilder
+import io.vertx.json.schema.SchemaParser
+import io.vertx.json.schema.common.dsl.Schemas.numberSchema
+
+@Endpoint
+class RejectServerConnectRequestAPI(
+    private val databaseManager: DatabaseManager,
+    setupManager: SetupManager,
+    authProvider: AuthProvider,
+    private val serverManager: ServerManager
+) : LoggedInApi(setupManager, authProvider) {
+    override val paths = listOf(Path("/api/servers/:id/reject", RouteType.POST))
+
+    override fun getValidationHandler(schemaParser: SchemaParser): ValidationHandler =
+        ValidationHandlerBuilder.create(schemaParser)
+            .pathParameter(param("id", numberSchema()))
+            .build()
+
+    override suspend fun handler(context: RoutingContext): Result {
+        val parameters = getParameters(context)
+        val id = parameters.pathParameter("id").long
+
+        val sqlConnection = createConnection(databaseManager, context)
+
+        val exists = databaseManager.serverDao.existsById(id, sqlConnection)
+
+        if (!exists) {
+            return Error(ErrorCode.NOT_EXISTS)
+        }
+
+        if (serverManager.isConnected(id)) {
+            serverManager.closeConnection(id)
+        }
+
+        databaseManager.serverDao.deleteById(id, sqlConnection)
+
+        return Successful()
+    }
+}
