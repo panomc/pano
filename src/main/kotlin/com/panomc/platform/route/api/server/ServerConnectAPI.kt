4c03cfb395c312a688b22ab44338155b7718e51c
kahverengi <ahmet@duruer.dev>
2022-12-17 01:36:35 +0300

    Add ServerConnectAPI

---
+++ src/main/kotlin/com/panomc/platform/route/api/server/ServerConnectAPI.kt
@@ -0,0 +1,115 @@
+package com.panomc.platform.route.api.server
+
+import com.panomc.platform.ErrorCode
+import com.panomc.platform.annotation.Endpoint
+import com.panomc.platform.db.DatabaseManager
+import com.panomc.platform.db.model.Server
+import com.panomc.platform.model.*
+import com.panomc.platform.server.ServerManager
+import com.panomc.platform.util.ServerAuthProvider
+import com.panomc.platform.util.ServerStatus
+import com.panomc.platform.util.SetupManager
+import io.vertx.core.http.ServerWebSocket
+import io.vertx.ext.web.RoutingContext
+import io.vertx.json.schema.SchemaParser
+import io.vertx.kotlin.coroutines.dispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+@Endpoint
+class ServerConnectAPI(
+    private val databaseManager: DatabaseManager,
+    private val setupManager: SetupManager,
+    private val serverAuthProvider: ServerAuthProvider,
+    private val serverManager: ServerManager
+) : Api() {
+    override val paths = listOf(Path("/api/server/connection", RouteType.GET))
+
+    override fun getValidationHandler(schemaParser: SchemaParser) = null
+
+    override suspend fun handler(context: RoutingContext): Result? {
+        val request = context.request()
+
+        request.pause()
+
+        if (!setupManager.isSetupDone()) {
+            return Error(ErrorCode.INSTALLATION_REQUIRED)
+        }
+
+        if (!serverAuthProvider.isAuthenticated(context)) {
+            return Error(ErrorCode.INVALID_TOKEN)
+        }
+
+        val serverId = serverAuthProvider.getServerIdFromRoutingContext(context)
+
+        val sqlConnection = databaseManager.createConnection()
+
+        val server = databaseManager.serverDao.getById(serverId, sqlConnection)
+
+        databaseManager.closeConnection(sqlConnection)
+
+        if (server == null) {
+            return Error(ErrorCode.INVALID_TOKEN)
+        }
+
+        if (!server.permissionGranted) {
+            return Error(ErrorCode.NEED_PERMISSION)
+        }
+
+        request.resume()
+
+        val webSocket = request.toWebSocket()
+
+        webSocket.onSuccess {
+            CoroutineScope(context.vertx().dispatcher()).launch {
+                onConnectionEstablished(context, it)
+            }
+        }
+
+        webSocket.onFailure {
+            if (!context.response().ended()) {
+                context.response().end()
+            }
+        }
+
+        return null
+    }
+
+    private suspend fun onConnectionEstablished(context: RoutingContext, serverWebSocket: ServerWebSocket) {
+        val serverId = serverAuthProvider.getServerIdFromRoutingContext(context)
+
+        val sqlConnection = databaseManager.createConnection()
+
+        val server = databaseManager.serverDao.getById(serverId, sqlConnection)!!
+
+        databaseManager.serverDao.updateStatusById(serverId, ServerStatus.ONLINE, sqlConnection)
+
+        databaseManager.closeConnection(sqlConnection)
+
+        serverManager.onServerConnect(server, serverWebSocket)
+
+        serverWebSocket.handler {
+            serverManager.onServerWrite(it)
+        }
+
+        serverWebSocket.closeHandler {
+            CoroutineScope(context.vertx().dispatcher()).launch {
+                onConnectionClosed(server)
+            }
+        }
+    }
+
+    private suspend fun onConnectionClosed(server: Server) {
+        val sqlConnection = databaseManager.createConnection()
+
+        val serverExists = databaseManager.serverDao.existsById(server.id, sqlConnection)
+
+        if (serverExists) {
+            databaseManager.serverDao.updateStatusById(server.id, ServerStatus.OFFLINE, sqlConnection)
+        }
+
+        databaseManager.closeConnection(sqlConnection)
+
+        serverManager.onServerDisconnect(server)
+    }
+}
