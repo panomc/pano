19595e117ce4497fb5b07332c68544092d912268
kahverengi <ahmet@duruer.dev>
2022-12-18 17:26:27 +0300

    Add byUserIdAndOption method to PanelConfigDao

--- src/main/kotlin/com/panomc/platform/db/dao/PanelConfigDao.kt
+++ src/main/kotlin/com/panomc/platform/db/dao/PanelConfigDao.kt
@@ -2,5 +2,12 @@ package com.panomc.platform.db.dao
 
 import com.panomc.platform.db.Dao
 import com.panomc.platform.db.model.PanelConfig
+import io.vertx.sqlclient.SqlConnection
 
-interface PanelConfigDao : Dao<PanelConfig>
+interface PanelConfigDao : Dao<PanelConfig> {
+    suspend fun byUserIdAndOption(
+        userId: Long,
+        option: String,
+        sqlConnection: SqlConnection
+    ): PanelConfig?
+}
--- src/main/kotlin/com/panomc/platform/db/implementation/PanelConfigDaoImpl.kt
+++ src/main/kotlin/com/panomc/platform/db/implementation/PanelConfigDaoImpl.kt
@@ -4,11 +4,16 @@ import com.panomc.platform.annotation.Dao
 import com.panomc.platform.db.DaoImpl
 import com.panomc.platform.db.DatabaseManager
 import com.panomc.platform.db.dao.PanelConfigDao
+import com.panomc.platform.db.model.PanelConfig
 import io.vertx.kotlin.coroutines.await
+import io.vertx.sqlclient.Row
+import io.vertx.sqlclient.RowSet
 import io.vertx.sqlclient.SqlConnection
+import io.vertx.sqlclient.Tuple
 
 @Dao
 class PanelConfigDaoImpl(databaseManager: DatabaseManager) : DaoImpl(databaseManager, "panel_config"), PanelConfigDao {
+
     override suspend fun init(sqlConnection: SqlConnection) {
         sqlConnection
             .query(
@@ -24,5 +29,23 @@ class PanelConfigDaoImpl(databaseManager: DatabaseManager) : DaoImpl(databaseMan
             )
             .execute()
             .await()
+    }
+
+    override suspend fun byUserIdAndOption(userId: Long, option: String, sqlConnection: SqlConnection): PanelConfig? {
+        val query =
+            "SELECT `id`, `option`, `value` FROM `${getTablePrefix() + tableName}` WHERE `user_id` = ? AND `option` = ?"
+
+        val rows: RowSet<Row> = sqlConnection
+            .preparedQuery(query)
+            .execute(Tuple.of(userId, option))
+            .await()
+
+        if (rows.size() == 0) {
+            return null
+        }
+
+        val row = rows.toList()[0]
+
+        return PanelConfig.from(row)
     }
 }
