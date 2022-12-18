adf163ec8da02148a0456c0754ca1fd5ba62e3ce
kahverengi <ahmet@duruer.dev>
2022-12-18 17:26:14 +0300

    Add table fields to PanelConfig model

--- src/main/kotlin/com/panomc/platform/db/model/PanelConfig.kt
+++ src/main/kotlin/com/panomc/platform/db/model/PanelConfig.kt
@@ -3,9 +3,19 @@ package com.panomc.platform.db.model
 import io.vertx.sqlclient.Row
 import io.vertx.sqlclient.RowSet
 
-data class PanelConfig(val id: Long = -1) {
+data class PanelConfig(
+    val id: Long = -1,
+    val userId: Long,
+    val option: String,
+    val value: String
+) {
     companion object {
-        fun from(row: Row) = PanelConfig(row.getLong(0))
+        fun from(row: Row) = PanelConfig(
+            row.getLong(0),
+            row.getLong(1),
+            row.getString(2),
+            row.getString(3),
+        )
 
         fun from(rowSet: RowSet<Row>) = rowSet.map { from(it) }
     }
