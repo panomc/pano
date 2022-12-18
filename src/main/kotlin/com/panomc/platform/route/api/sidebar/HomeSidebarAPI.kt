3f3d4f63948f6e19395ed12e236c86796fe187bb
kahverengi <ahmet@duruer.dev>
2022-12-18 19:39:24 +0300

    Add returning main server data in HomeSidebarAPI

--- src/main/kotlin/com/panomc/platform/route/api/sidebar/HomeSidebarAPI.kt
+++ src/main/kotlin/com/panomc/platform/route/api/sidebar/HomeSidebarAPI.kt
@@ -3,6 +3,8 @@ package com.panomc.platform.route.api.sidebar
 import com.panomc.platform.annotation.Endpoint
 import com.panomc.platform.config.ConfigManager
 import com.panomc.platform.db.DatabaseManager
+import com.panomc.platform.db.model.Server
+import com.panomc.platform.db.model.SystemProperty
 import com.panomc.platform.model.*
 import io.vertx.ext.web.RoutingContext
 import io.vertx.ext.web.validation.ValidationHandler
@@ -22,9 +24,25 @@ class HomeSidebarAPI(private val configManager: ConfigManager, private val datab
         val response = mutableMapOf<String, Any?>()
 
         response["ipAddress"] = configManager.getConfig().getString("server-ip-address")
+        response["serverGameVersion"] = configManager.getConfig().getString("server-game-version")
 
         val sqlConnection = createConnection(databaseManager, context)
 
+        val mainServerId = databaseManager.systemPropertyDao.getValue(
+            SystemProperty(option = "main_server"),
+            sqlConnection
+        )?.value?.toLong()
+        var mainServer: Server? = null
+
+        if (mainServerId != null && mainServerId != -1L) {
+            mainServer = databaseManager.serverDao.getById(mainServerId, sqlConnection)
+        }
+
+        response["mainServer"] = if (mainServer == null) null else mapOf<String, Any?>(
+            "playerCount" to mainServer.playerCount,
+            "maxPlayerCount" to mainServer.maxPlayerCount,
+            "status" to mainServer.status
+        )
         response["lastRegisteredUsers"] = databaseManager.userDao.getLastUsernames(12, sqlConnection)
 
         return Successful(response)
