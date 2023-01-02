c4dfc88f899b66a73d85ee90f8387060e4865d47
kahverengi <ahmet@duruer.dev>
2023-01-02 23:05:19 +0300

    Add OnPlayerDisconnect server event

---
+++ src/main/kotlin/com/panomc/platform/server/event/OnPlayerDisconnect.kt
@@ -0,0 +1,27 @@
+package com.panomc.platform.server.event
+
+import com.panomc.platform.annotation.Event
+import com.panomc.platform.db.DatabaseManager
+import com.panomc.platform.db.model.Server
+import com.panomc.platform.server.ServerEvent
+import com.panomc.platform.server.ServerEventListener
+import io.vertx.core.json.JsonObject
+
+@Event
+class OnPlayerDisconnect(private val databaseManager: DatabaseManager) : ServerEventListener {
+    override val serverEvent: ServerEvent = ServerEvent.ON_PLAYER_DISCONNECT
+
+    override suspend fun handle(body: JsonObject, server: Server) {
+        val player = body.getJsonObject("player")
+
+        val sqlConnection = databaseManager.createConnection()
+
+        databaseManager.serverPlayerDao.deleteByUsernameAndServerId(
+            player.getString("username"),
+            server.id,
+            sqlConnection
+        )
+
+        databaseManager.closeConnection(sqlConnection)
+    }
+}
