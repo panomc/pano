f5948d53e8d44c3ebcfa5dd5db036300c98024f6
kahverengi <ahmet@duruer.dev>
2023-01-03 22:21:32 +0300

    Add SupportSidebarAPI

---
+++ src/main/kotlin/com/panomc/platform/route/api/sidebar/SupportSidebarAPI.kt
@@ -0,0 +1,28 @@
+package com.panomc.platform.route.api.sidebar
+
+import com.panomc.platform.annotation.Endpoint
+import com.panomc.platform.db.DatabaseManager
+import com.panomc.platform.model.*
+import io.vertx.ext.web.RoutingContext
+import io.vertx.ext.web.validation.ValidationHandler
+import io.vertx.ext.web.validation.builder.ValidationHandlerBuilder
+import io.vertx.json.schema.SchemaParser
+
+@Endpoint
+class SupportSidebarAPI(private val databaseManager: DatabaseManager) : Api() {
+    override val paths = listOf(Path("/api/sidebar/support", RouteType.GET))
+
+    override fun getValidationHandler(schemaParser: SchemaParser): ValidationHandler =
+        ValidationHandlerBuilder.create(schemaParser)
+            .build()
+
+    override suspend fun handler(context: RoutingContext): Result {
+        val response = mutableMapOf<String, Any?>()
+
+        val sqlConnection = createConnection(context)
+
+        response["onlineAdmins"] = databaseManager.userDao.getOnlineAdmins(-1, sqlConnection).map { it.username }
+
+        return Successful(response)
+    }
+}
