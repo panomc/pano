7f6a8d883e7f2fce1a9b47c4e2fb8e9e43044896
kahverengi <ahmet@duruer.dev>
2023-01-07 23:33:08 +0300

    Add permission requirement for response in PanelGetBasicDataAPI

--- src/main/kotlin/com/panomc/platform/route/api/panel/PanelGetBasicDataAPI.kt
+++ src/main/kotlin/com/panomc/platform/route/api/panel/PanelGetBasicDataAPI.kt
@@ -3,6 +3,7 @@ package com.panomc.platform.route.api.panel
 import com.panomc.platform.ErrorCode
 import com.panomc.platform.annotation.Endpoint
 import com.panomc.platform.auth.AuthProvider
+import com.panomc.platform.auth.PanelPermission
 import com.panomc.platform.config.ConfigManager
 import com.panomc.platform.db.DatabaseManager
 import com.panomc.platform.db.model.Permission
@@ -35,50 +36,54 @@ class PanelGetBasicDataAPI(
 
         val count = databaseManager.panelNotificationDao.getCountOfNotReadByUserId(userId, sqlConnection)
 
-        val mainServerId = databaseManager.systemPropertyDao.getByOption(
-            "main_server",
-            sqlConnection
-        )?.value?.toLong()
-        var mainServer: Server? = null
-
-        if (mainServerId != null && mainServerId != -1L) {
-            mainServer = databaseManager.serverDao.getById(mainServerId, sqlConnection)
-        }
-
-        val selectedServerPanelConfig =
-            databaseManager.panelConfigDao.byUserIdAndOption(userId, "selected_server", sqlConnection)
-        var selectedServer: Server? = null
-
-        if (selectedServerPanelConfig != null) {
-            val selectedServerId = selectedServerPanelConfig.value.toLong()
-
-            selectedServer = databaseManager.serverDao.getById(selectedServerId, sqlConnection)
-        }
-
 //        Since it's a panel API, it calls AuthProvider#hasAccessPanel method and these context fields are created
         val isAdmin = context.get<Boolean>("isAdmin") ?: false
         val permissions = context.get<List<Permission>>("permissions") ?: listOf()
 
-        return Successful(
-            mapOf(
-                "user" to mapOf(
-                    "username" to user.username,
-                    "email" to user.email,
-                    "permissions" to permissions.map { it.name },
-                    "admin" to isAdmin
-                ),
-                "website" to mapOf(
-                    "name" to configManager.getConfig().getString("website-name"),
-                    "description" to configManager.getConfig().getString("website-description")
-                ),
-                "platformServerMatchKey" to platformCodeManager.getPlatformKey(),
-                "platformServerMatchKeyTimeStarted" to platformCodeManager.getTimeStarted(),
-                "platformHostAddress" to context.request().host(),
-                "mainServer" to mainServer,
-                "selectedServer" to selectedServer,
-                "notificationCount" to count,
-                "locale" to configManager.getConfig().getString("locale")
-            )
+        val result = mutableMapOf(
+            "user" to mapOf(
+                "username" to user.username,
+                "email" to user.email,
+                "permissions" to permissions.map { it.name },
+                "admin" to isAdmin
+            ),
+            "website" to mapOf(
+                "name" to configManager.getConfig().getString("website-name"),
+                "description" to configManager.getConfig().getString("website-description")
+            ),
+            "notificationCount" to count,
+            "locale" to configManager.getConfig().getString("locale")
         )
+
+        if (authProvider.hasPermission(userId, PanelPermission.MANAGE_SERVERS, context)) {
+            val mainServerId = databaseManager.systemPropertyDao.getByOption(
+                "main_server",
+                sqlConnection
+            )?.value?.toLong()
+            var mainServer: Server? = null
+
+            if (mainServerId != null && mainServerId != -1L) {
+                mainServer = databaseManager.serverDao.getById(mainServerId, sqlConnection)
+            }
+
+            val selectedServerPanelConfig =
+                databaseManager.panelConfigDao.byUserIdAndOption(userId, "selected_server", sqlConnection)
+            var selectedServer: Server? = null
+
+            if (selectedServerPanelConfig != null) {
+                val selectedServerId = selectedServerPanelConfig.value.toLong()
+
+                selectedServer = databaseManager.serverDao.getById(selectedServerId, sqlConnection)
+            }
+
+            result["platformServerMatchKey"] = platformCodeManager.getPlatformKey()
+            result["platformServerMatchKeyTimeStarted"] = platformCodeManager.getTimeStarted()
+            result["platformHostAddress"] = context.request().host()
+
+            result["mainServer"] = mainServer
+            result["selectedServer"] = selectedServer
+        }
+
+        return Successful(result)
     }
 }
