d83f86eb8b778e9f68a5f36e7cbce2eb879cdf91
kahverengi <ahmet@duruer.dev>
2023-01-08 21:45:22 +0300

    Add SendResetPasswordEmailAPI

---
+++ src/main/kotlin/com/panomc/platform/route/api/profile/SendResetPasswordEmailAPI.kt
@@ -0,0 +1,44 @@
+package com.panomc.platform.route.api.profile
+
+import com.panomc.platform.ErrorCode
+import com.panomc.platform.annotation.Endpoint
+import com.panomc.platform.auth.AuthProvider
+import com.panomc.platform.db.DatabaseManager
+import com.panomc.platform.mail.MailManager
+import com.panomc.platform.mail.mails.ResetPasswordMail
+import com.panomc.platform.model.*
+import com.panomc.platform.token.TokenType
+import io.vertx.ext.web.RoutingContext
+import io.vertx.json.schema.SchemaParser
+
+@Endpoint
+class SendResetPasswordEmailAPI(
+    private val databaseManager: DatabaseManager,
+    private val mailManager: MailManager,
+    private val authProvider: AuthProvider
+) : LoggedInApi() {
+    override val paths = listOf(Path("/api/profile/resetPassword", RouteType.POST))
+
+    override fun getValidationHandler(schemaParser: SchemaParser) = null
+
+    override suspend fun handle(context: RoutingContext): Result {
+        val userId = authProvider.getUserIdFromRoutingContext(context)
+
+        val sqlConnection = createConnection(context)
+
+        val lastToken =
+            databaseManager.tokenDao.getLastBySubjectAndType(userId.toString(), TokenType.RESET_PASSWORD, sqlConnection)
+
+        if (lastToken != null) {
+            val fifteenMinutesLaterInMillis = lastToken.startDate + 15 * 60 * 1000
+
+            if (System.currentTimeMillis() < fifteenMinutesLaterInMillis) {
+                throw Error(ErrorCode.CANT_RESET_PASSWORD_WAIT_15_MINUTES)
+            }
+        }
+
+        mailManager.sendMail(sqlConnection, userId, ResetPasswordMail())
+
+        return Successful()
+    }
+}
