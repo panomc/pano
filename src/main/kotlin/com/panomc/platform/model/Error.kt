0fead6a1998863f6c50aed86927d05a693ee0cdc
kahverengi <ahmet@duruer.dev>
2023-10-02 15:34:54 +0300

    Add extras option to error object

--- src/main/kotlin/com/panomc/platform/model/Error.kt
+++ src/main/kotlin/com/panomc/platform/model/Error.kt
@@ -3,7 +3,8 @@ package com.panomc.platform.model
 import com.panomc.platform.ErrorCode
 import com.panomc.platform.model.Result.Companion.encode
 
-open class Error(private val errorCode: ErrorCode) : Throwable(errorCode.toString()), Result {
+open class Error(private val errorCode: ErrorCode, private val extras: Map<String, Any?> = mapOf()) :
+    Throwable(errorCode.toString()), Result {
 
     override fun encode(extras: Map<String, Any?>): String {
         val response = mutableMapOf<String, Any?>(
@@ -11,6 +12,7 @@ open class Error(private val errorCode: ErrorCode) : Throwable(errorCode.toStrin
             "error" to errorCode
         )
 
+        response.putAll(this.extras)
         response.putAll(extras)
 
         return response.encode()
