6723627c12bda0ff3a88c65943a8baf36cfd3281
kahverengi <ahmet@duruer.dev>
2024-03-25 20:17:45 +0300

    feat: add bean instances for easy access on dependency injection usage

--- Pano/src/main/kotlin/com/panomc/platform/PluginFactory.kt
+++ Pano/src/main/kotlin/com/panomc/platform/PluginFactory.kt
@@ -16,23 +16,10 @@ class PluginFactory(private val pluginEventManager: PluginEventManager, private 
     }
 
     override fun createInstance(pluginClass: Class<*>, pluginWrapper: PluginWrapper): Plugin? {
-        val pluginBeanContext by lazy {
-            val pluginBeanContext = AnnotationConfigApplicationContext()
-
-            pluginBeanContext.parent = Main.applicationContext
-            pluginBeanContext.classLoader = pluginClass.classLoader
-            pluginBeanContext.scan(pluginClass.`package`.name)
-            pluginBeanContext.refresh()
-
-            pluginBeanContext
-        }
-
         try {
             val constructor = pluginClass.getConstructor()
 
             val plugin = constructor.newInstance() as PanoPlugin
-
-            pluginEventManager.initializePlugin(plugin, pluginBeanContext)
 
             pluginUiManager.initializePlugin(plugin)
 
@@ -42,8 +29,21 @@ class PluginFactory(private val pluginEventManager: PluginEventManager, private 
             plugin.pluginUiManager = pluginUiManager
             plugin.environmentType = Main.ENVIRONMENT
             plugin.releaseStage = Main.STAGE
-            plugin.pluginBeanContext = pluginBeanContext
             plugin.applicationContext = Main.applicationContext
+
+            val pluginBeanContext by lazy {
+                val pluginBeanContext = AnnotationConfigApplicationContext()
+
+                pluginBeanContext.parent = Main.applicationContext
+                pluginBeanContext.classLoader = pluginClass.classLoader
+                pluginBeanContext.scan(pluginClass.`package`.name)
+                pluginBeanContext.refresh()
+
+                pluginBeanContext
+            }
+            plugin.pluginBeanContext = pluginBeanContext
+
+            pluginEventManager.initializePlugin(plugin, pluginBeanContext)
 
             runBlocking {
                 plugin.onLoad()
